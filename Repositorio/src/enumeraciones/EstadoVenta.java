e17aa0801933e838b5575190656072220a017747
fede <fede@192.168.1.16>
2019-06-10 15:51:17 -0300

    Se elimina PENDIENTE de EstadoVenta

--- Repositorio/src/enumeraciones/EstadoVenta.java
+++ Repositorio/src/enumeraciones/EstadoVenta.java
@@ -3,8 +3,7 @@ package enumeraciones;
 public enum EstadoVenta {
 	ABIERTA (1, "Abierta"),
 	FACTURADA (2, "Facturada"),
-	PENDIENTE (3, "Pendiente"),
-	CANCELADA (4, "Cancelada");
+	CANCELADA (3, "Cancelada");
 	
 	private final int id;
 	private final String nombre;
