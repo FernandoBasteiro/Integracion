2c165084601ab86f10e337216f25df35d7dd6f99
Tomas <tmonteroswinnen@gmail.com>
2019-06-10 16:56:17 -0300

    entities

--- Servidor/src/controladores/ControladorEmpleados.java
+++ Servidor/src/controladores/ControladorEmpleados.java
@@ -14,31 +14,31 @@ public class ControladorEmpleados {
 		return e;
 	}
 	
-	public  void altaEmpleado(EmpleadoDTO e) {
+	public  void altaEmpleado(EmpleadoDTO gerente, EmpleadoDTO e) {
 			
 	}
 	
-	public  void modificacionEmpleado(EmpleadoDTO e) {
+	public  void modificacionEmpleado(EmpleadoDTO gerente, EmpleadoDTO e) {
 		
 	}
 	
-	public  void bajaEmpleado(EmpleadoDTO e) {
+	public  void bajaEmpleado(EmpleadoDTO gerente, EmpleadoDTO e) {
 		
 	}
 	
-	public EmpleadoDTO mostrarFichaEmpleado(EmpleadoDTO e) {
+	public EmpleadoDTO mostrarFichaEmpleado(EmpleadoDTO gerente, EmpleadoDTO e) {
 		return e;
 	}
 	
-	public ArrayList<EmpleadoDTO> listarEmpleados() {
+	public ArrayList<EmpleadoDTO> listarEmpleados(EmpleadoDTO supervisor) {
 		return null;
 	}
 	
-	public ArrayList<EmpleadoDTO> listarEmpleados(EstadoEmpleado ee) {
+	public ArrayList<EmpleadoDTO> listarEmpleados(EmpleadoDTO supervisor, EstadoEmpleado ee) {
 		return null;
 	}
 	
-	public ArrayList<EmpleadoDTO> listarEmpleados(Puesto p) {
+	public ArrayList<EmpleadoDTO> listarEmpleados(EmpleadoDTO supervisor, Puesto p) {
 		return null;
 	}
 	
---
+++ Servidor/src/entities/EmpleadoEntity.java
@@ -0,0 +1,10 @@
+package entities;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+
+public class EmpleadoEntity {
+
+}
