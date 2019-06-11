551089e7573e1775f579a8b218fd735a79943451
Fernando Basteiro <fernando@basteiro.com.ar>
2019-06-11 08:34:53 -0300

    Constructores Getters y Setters de DTO

--- Repositorio/src/dto/ProductoDTO.java
+++ Repositorio/src/dto/ProductoDTO.java
@@ -7,4 +7,53 @@ public class ProductoDTO {
 	private String presentacion;
 	private Float precio;
 	private StockDTO stock;
+	public Integer getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public String getPresentacion() {
+		return presentacion;
+	}
+	public void setPresentacion(String presentacion) {
+		this.presentacion = presentacion;
+	}
+	public Float getPrecio() {
+		return precio;
+	}
+	public void setPrecio(Float precio) {
+		this.precio = precio;
+	}
+	public StockDTO getStock() {
+		return stock;
+	}
+	public void setStock(StockDTO stock) {
+		this.stock = stock;
+	}
+	public ProductoDTO(Integer codigo, String nombre, String descripcion, String presentacion, Float precio,
+			StockDTO stock) {
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.presentacion = presentacion;
+		this.precio = precio;
+		this.stock = stock;
+	}
+	public ProductoDTO() {
+	}
+	
+	
 }
--- Repositorio/src/dto/VentaDTO.java
+++ Repositorio/src/dto/VentaDTO.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import enumeraciones.EstadoVenta;
+import enumeraciones.MedioDePago;
 
 public class VentaDTO {
 	private Integer id;
@@ -12,4 +13,61 @@ public class VentaDTO {
 	private EmpleadoDTO empleado;
 	private EstadoVenta estado;
 	private Float total;
+	private MedioDePago medioDePago;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public LocalDateTime getFechaVenta() {
+		return fechaVenta;
+	}
+	public void setFechaVenta(LocalDateTime fechaVenta) {
+		this.fechaVenta = fechaVenta;
+	}
+	public List<ItemVentaDTO> getItems() {
+		return items;
+	}
+	public void setItems(List<ItemVentaDTO> items) {
+		this.items = items;
+	}
+	public EmpleadoDTO getEmpleado() {
+		return empleado;
+	}
+	public void setEmpleado(EmpleadoDTO empleado) {
+		this.empleado = empleado;
+	}
+	public EstadoVenta getEstado() {
+		return estado;
+	}
+	public void setEstado(EstadoVenta estado) {
+		this.estado = estado;
+	}
+	public Float getTotal() {
+		return total;
+	}
+	public void setTotal(Float total) {
+		this.total = total;
+	}
+	public MedioDePago getMedioDePago() {
+		return medioDePago;
+	}
+	public void setMedioDePago(MedioDePago medioDePago) {
+		this.medioDePago = medioDePago;
+	}
+	public VentaDTO(Integer id, LocalDateTime fechaVenta, List<ItemVentaDTO> items, EmpleadoDTO empleado,
+			EstadoVenta estado, Float total, MedioDePago medioDePago) {
+		this.id = id;
+		this.fechaVenta = fechaVenta;
+		this.items = items;
+		this.empleado = empleado;
+		this.estado = estado;
+		this.total = total;
+		this.medioDePago = medioDePago;
+	}
+	public VentaDTO() {
+	}
+	
+	
 }
