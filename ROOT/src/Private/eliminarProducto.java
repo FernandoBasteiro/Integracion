c24da4eba1043ab773659501d76ef81b49b87684
Erica Nuñez <erikannunez@gmail.com>
2019-06-13 20:43:05 -0300

    ROOT

---
+++ ROOT/src/Private/eliminarProducto.java
@@ -0,0 +1,52 @@
+package Private;
+
+import java.io.IOException;
+
+import javax.json.Json;
+import javax.json.JsonObjectBuilder;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import delegado.BusinessDelegate;
+import dto.EmpleadoDTO;
+import dto.ProductoDTO;
+import excepciones.ComunicacionException;
+import excepciones.ExcepcionProceso;
+import excepciones.UsuarioNoLogueado;
+import excepciones.UsuarioSinPermisos;
+
+@WebServlet("/Private/eliminarProducto")
+public class eliminarProducto extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		JsonObjectBuilder json = Json.createObjectBuilder();
+		try {
+			BusinessDelegate bd = BusinessDelegate.getInstance();
+			HttpSession session = request.getSession();
+			EmpleadoDTO logged = (EmpleadoDTO) session.getAttribute("loggedUsr");
+			String codigoStr = request.getParameter("codigo");
+			Long codigo = null;
+			if (codigoStr != null) codigo = Long.valueOf(codigoStr);
+			ProductoDTO p = new ProductoDTO();
+			p.setCodigo(codigo);
+			bd.bajaProducto(logged, p);
+			json.add("success", "El producto fue dado de baja.");
+		}
+		catch (ComunicacionException | UsuarioNoLogueado | ExcepcionProceso | UsuarioSinPermisos e) {
+			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			json.add("error", e.getMessage());
+		}
+		response.setContentType("application/json");
+		response.getWriter().write(json.build().toString());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
